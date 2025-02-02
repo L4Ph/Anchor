f0cd5b314c4a07eec1b0bd5fddfd09ab1b6d52ca
L4Ph <4ranci0ne@gmail.com>
2025-02-02 19:32:53 +0900

    apply unocss

---
+++ static/unocss-config.js
@@ -0,0 +1,13 @@
+window.__unocss = {
+    presets: [
+      () =>
+        window.__unocss_runtime.presets.presetWebFonts({
+          fonts: {
+            provider: 'google',
+            fonts: {
+              sans: 'Roboto',
+            },
+          }
+        }),
+    ],
+  }
